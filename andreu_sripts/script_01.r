08be8fad32ecce435ac1b793655f34a33fc7c6ef
AndreuJove <andreujove@gmail.com>
2021-01-09 15:31:33 +0100

    2 new scripts

---
+++ andreu_sripts/script_01.r
@@ -0,0 +1,8 @@
+x = (pi^2)/2
+
+variable_1 = 4
+
+
+doble = function(x){2*x}
+
+doble(vvariable_1)
---
+++ andreu_sripts/script_02.r
@@ -0,0 +1,24 @@
+x = (pi^2)/2
+
+variable_1 = 4
+
+resta = function(x, y){
+  print(paste(sprintf("%i - %i = ", x, y), x-y))
+  }
+
+resta(5,2)
+
+
+## Funcion f(x) = x^3 - (3^x) * sen(x)
+
+doble(variable_1)
+
+
+exp(2)
+
+ls()
+
+rm(list = ls())
+
+
+
